5cc2267c8bd25ff4c51df297b726657cb06726e5
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2020-10-24 15:22:01 -0300

    refactor(model types): rename and comment model types

--- src/types/model.ts
+++ src/types/model.ts
@@ -2,53 +2,109 @@ import Model from '../Model'
 
 /**
  * Domain Model
+ * ---
  *
- * Methods using this type as response: `$first` and `$find`
+ * The Domain Model type only returns the properties and methods that belongs to domain models,
+ * with the addition of [Model Operations]{@link ModelOperations}.
+ * All other methods from the base [Base Model]{@link Model} are stripped from this type.
  *
- * The domain model type only returns the properties and methods that belongs to the domain model,
- * with the addition of model operators. All other methods from the base model are stripped from this type.
+ * **T - Model**
+ *
  */
-
-type ModelOperations = 'delete' | 'save' | 'attach' | 'sync' | 'for'
 
 export type DomainModel<T extends Model<boolean, boolean>> = Required<
   Omit<T, keyof Omit<Model, ModelOperations>>
 >
 
 /**
- * Model and Collection Types
+ * Model Operations
+ * ---
  *
- * Query Builder methods using these types: `first`, `find`, `get`, `$get` and `save` (`_create` and `_update`)
- *
- * These types should be used internally for type checking. They are Union types.
- * The type guards `isWrappedModel` and `isWrappedCollection` in Model class can be used to predict the type.
- * They shouldn't be used for response. They must be converted to the Conditional Types.
- *
- * TModel as QueryResponseModel
- * TCollection as QueryResponseCollection
+ * Operation methods that shouldn't be stripped from [Domain Model]{@link DomainModel} type.
  *
  */
 
+type ModelOperations = 'delete' | 'save' | 'attach' | 'sync' | 'for'
+
+/**
+ * Model and Collection Types
+ * ---
+ *
+ * Query Builder: [`first`]{@link Model#first}, [`find`]{@link Model#find}, [`get`]{@link Model#get},
+ * [`$get`]{@link Model#$get} and [`save`]{@link Model#save}
+ * ([`_create`]{@link Model#_create} and [`_update`]{@link Model#_update})
+ *
+ * These types should be used internally for type checking. They are Union types.
+ * The type guards [`isWrappedModel`]{@link Model#isWrappedModel}
+ * and [`isWrappedCollection`]{@link Model#isWrappedCollection}` in Model class can be used to predict the type.
+ *
+ * They shouldn't be used for response. They must be converted to the Conditional Types.
+ *
+ * {@link TModel} as {@link QueryResponseModel}
+ *
+ * {@link TCollection} as {@link QueryResponseCollection}
+ *
+ */
+
+/**
+ * Wrapped Model Type
+ * ---
+ *
+ * A model inside `data` wrapper.
+ *
+ * @property {Model} T - The model of the response.
+ *
+ */
 export type WrappedModel<T extends Model<boolean, boolean>> = {
   data: DomainModel<T>
 }
 
+/**
+ * Model Type
+ * ---
+ *
+ * A model that can be wrapped or not.
+ *
+ * @property {Model} T - The model of the response.
+ *
+ */
 export type TModel<T extends Model<boolean, boolean>> =
   | DomainModel<T>
   | WrappedModel<T>
 
+/**
+ * Wrapped Collection Type
+ * ---
+ *
+ * A collection of models inside `data` wrapper.
+ *
+ * @property {Model} T - The model of the response.
+ *
+ */
 export type WrappedCollection<T extends Model<boolean, boolean>> = {
   data: TModel<T>[]
 }
 
+/**
+ * Collection Type
+ * ---
+ *
+ * A collection of models that can be wrapped or not.
+ *
+ * @property {Model} T - The model of the response.
+ *
+ */
 export type TCollection<T extends Model<boolean, boolean>> =
   | WrappedCollection<T>
   | TModel<T>[]
 
 /**
  * Model and Collection Response Types
+ * ---
  *
- * Query Builder methods using these types as response: `first`, `find`, `get`, `$get` and `save` (`_create` and `_update`)
+ * Query Builder: [`first`]{@link Model#first}, [`find`]{@link Model#find}, [`get`]{@link Model#get},
+ * [`$get`]{@link Model#$get} and [`save`]{@link Model#save}
+ * ([`_create`]{@link Model#_create} and [`_update`]{@link Model#_update})
  *
  * These are types used for responses. Different from the types above, these are Conditional Types.
  * They use the arguments of the Model class (Generic Class) to define if the collection and the model
@@ -57,23 +113,42 @@ export type TCollection<T extends Model<boolean, boolean>> =
  * These types can't be used internally for type checking, only for responses.
  * When using methods that returns these types internally, they must be converted to Union Types.
  *
- * QueryResponseModel as TModel
- * QueryResponseCollection as TCollection
+ * {@link QueryResponseModel} as {@link TModel}
  *
- * C = Is Wrapped Collection?
- * M = Is Wrapped Model?
+ * {@link QueryResponseCollection} as {@link TCollection}
  *
  */
 
+/**
+ * Model Response Type
+ * ---
+ * A model that is wrapped or not, based on the **M** property.
+ *
+ * @property {Model} T - The model of the response.
+ * @property {boolean} M = false - Is the model wrapped?
+ *
+ */
 export type QueryResponseModel<
   T extends Model<boolean, boolean>,
-  M
+  M extends boolean = false
 > = M extends true ? WrappedModel<T> : DomainModel<T>
 
+/**
+ * Collection Response Type
+ * ---
+ *
+ * A collection of models that are wrapped or not, based on the **C** property.
+ * The models are wrapped or not, based on the **M** property.
+ *
+ * @property {Model} T - The model of the response.
+ * @property {boolean} C = false - Is the collection of models wrapped?
+ * @property {boolean} M = false - Is the model wrapped?
+ *
+ */
 export type QueryResponseCollection<
   T extends Model<boolean, boolean>,
-  C,
-  M
+  C extends boolean = false,
+  M extends boolean = false
 > = C extends true
   ? { data: QueryResponseModel<T, M>[] }
   : QueryResponseModel<T, M>[]
